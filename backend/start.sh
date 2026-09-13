36739b05841d14b62b4b3bb5f3d3209f5b5b272f
KRISHNA SRIVASTAVA <ksrivastava9199@gmail.com>
2026-09-13 15:55:18 +0530

    Fix Gunicorn single-worker deadlock and add threads

--- backend/start.sh
+++ backend/start.sh
@@ -6,4 +6,4 @@ python manage.py collectstatic --noinput
 # Start the Celery worker in the background
 celery -A symbiosis_proj worker -l info &
 # Start the Gunicorn web server in the foreground
-gunicorn symbiosis_proj.wsgi:application --bind 0.0.0.0:${PORT:-8000}
+gunicorn symbiosis_proj.wsgi:application --bind 0.0.0.0:${PORT:-8000} --threads 4
